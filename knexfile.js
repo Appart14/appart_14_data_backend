f222fc3cdfc3f2c065205c8eea872e66eb88e726
Nicolas Patenaude <1749394@cegepgranby.qc.ca>
2021-05-05 00:01:27 -0400

    parametres de connexion ajoutés et migration exécutée

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,18 @@ dotenv.config();
 export default {
   development: {
     client: 'mysql',
+    /*
     connection: {
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
+    },
+    */
+    connection: {
+      host: "192.168.0.110",
+      user: "root",
+      password: "password",
+      database: "appart_14",
     },
     migrations: {
       directory: './db/migrations',
